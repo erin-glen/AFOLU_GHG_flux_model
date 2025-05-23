ac0fa1bba13311f1b3842dbf83b5b618710ee85a
erin-glen <erin.glen@wri.org>
2025-05-23 12:49:53 -0600

    Update SDPT constants

--- src/scripts/utilities/constants_and_names.py
+++ src/scripts/utilities/constants_and_names.py
@@ -82,7 +82,7 @@ patterns = {
     'grip': "{tile_id}_grip_density.tif",
     'osm_roads': "{tile_id}_osm_roads_density.tif",
     'osm_canals': "{tile_id}_osm_canals_density.tif",
-    'planted_forest_type': "{tile_id}_plantations.tif",
+    'planted_forest_type': "{tile_id}_sdpt.tif",
     'extraction': "{tile_id}_extraction.tif",
     'climate_domain': "{tile_id}_fao_ecozones_bor_tem_tro_processed.tif",
     'descals_type': "plantation_type_{tile_id}.tif",
@@ -101,7 +101,7 @@ dirs = {
     'grip': posixpath.join(full_bucket_prefix, processed_dir, 'grip_density/40000_pixels/20240925'),
     'osm_roads': posixpath.join(full_bucket_prefix, processed_dir, 'osm_roads_density/40000_pixels/20240925'),
     'osm_canals': posixpath.join(full_bucket_prefix, processed_dir, 'osm_canals_density/40000_pixels/20240822'),
-    'planted_forest_type': posixpath.join(full_bucket_prefix, processed_dir, 'sdpt'),
+    'planted_forest_type': posixpath.join(full_bucket_prefix, processed_dir, 'sdpt/40000_pixels/20240925'),
     'extraction': posixpath.join(full_bucket_prefix, processed_dir, 'extraction/20241021'),
     'climate_domain': posixpath.join(full_bucket_prefix, 'climate/carbon_model/inputs_for_carbon_pools/processed/fao_ecozones_bor_tem_tro/20190418'),
     'descals_type': posixpath.join(full_bucket_prefix, processed_dir, 'descals_plantation/extent/20241105'),
@@ -228,4 +228,3 @@ if __name__ == "__main__":
     test_dict = get_dynamic_download_dict('00N_110E', 2015)
     for k, v in test_dict.items():
         print(f"{k}: {v}")
-
