40881a5fb09de5433ac01aefd0625997277830b7
erin-glen <erin.glen@wri.org>
2025-06-06 17:24:47 -0600

    Updated dates

--- src/scripts/postprocessing/create_aggregation_list.py
+++ src/scripts/postprocessing/create_aggregation_list.py
@@ -1,5 +1,5 @@
 pixel_resolution = '{pixel_resolution}'
-output_date = '20250605'
+output_date = '20250606'
 
 inventory_periods = [
     '2000_2005',
@@ -14,6 +14,7 @@ data_types = [
     'burned_co2',
     'burned_co_co2e',
     'burned_state',
+    'burned_emission_state',
     'burned_total_co2e',
     'drained_ch4_ditch_co2e',
     'drained_ch4_land_co2e',
@@ -21,11 +22,12 @@ data_types = [
     'drained_co2_offsite',
     'drained_n2o_co2e',
     'drained_total_co2e',
+    'emission_state',
     'soil',
     'state'
 ]
 
-base_url = "s3://gfw2-data/climate/AFOLU_flux_model/organic_soils/outputs/version_0_3_5"
+base_url = "s3://gfw2-data/climate/AFOLU_flux_model/organic_soils/outputs/version_0_3_7"
 
 paths = []
 
