747dcc95996221df7ced37ebf58799e75d8d62ff
erin-glen <erin.glen@wri.org>
2025-08-18 16:10:48 -0600

    Changed default threshold to 1km

--- src/scripts/core_model/0_drainage_emissions_model.py
+++ src/scripts/core_model/0_drainage_emissions_model.py
@@ -219,10 +219,10 @@ def calculate_drainage_and_emissions(
             # A) Drainage classification ----------------------------------
             if peat == 1:
                 node = nu.accrete_node(node, 1)
-                if dadap > 0 or osm_canals > 0:
+                if dadap > 1 or osm_canals > 1:
                     node = nu.accrete_node(node, 1)
                     drained = True
-                elif engert > 0 or grip > 0 or osm_roads > 0:
+                elif engert > 1 or grip > 1 or osm_roads > 1:
                     node = nu.accrete_node(node, 2)
                     drained = True
                 elif land_cover in (cropland_code, settlement_code):
