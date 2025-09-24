b247b3364b8edc66fbf13d352055b89d348ea071
erin-glen <erin.glen@wri.org>
2025-09-24 13:18:18 -0600

    Update date

--- src/scripts/core_model/2_per_pixel_soils_outputs.py
+++ src/scripts/core_model/2_per_pixel_soils_outputs.py
@@ -38,7 +38,7 @@ INVENTORY_PERIODS = [
 
 version = cn.model_version_underscore
 BASE_URL = f"s3://gfw2-data/climate/AFOLU_flux_model/organic_soils/outputs/version_{version}"
-OUTPUT_DATE = "20250914"
+OUTPUT_DATE = "20250923"
 PIXEL_RES = "4000_pixels"
 
 
