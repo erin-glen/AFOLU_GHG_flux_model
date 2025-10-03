436a00bb71224ef89926a51d4e3751259abdff9b
dagibbs22 <dagibbs22@gmail.com>
2025-10-02 22:36:34 -0400

    Starting branch v1.0.2.

--- src/utilities/constants_and_names.py
+++ src/utilities/constants_and_names.py
@@ -7,8 +7,7 @@ import numpy as np
 ########
 
 ### Model version
-# model_version = "1.0.1"
-model_version = "1.0.1"
+model_version = "1.0.2"
 model_version_underscore = model_version.replace(".", "_")
 
 ### s3 buckets
