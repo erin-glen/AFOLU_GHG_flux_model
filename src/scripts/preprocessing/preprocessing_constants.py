c5cafad98a09f4b384f015b6b46d64c8b4294d1e
erin-glen <erin.glen@wri.org>
2025-10-17 12:04:55 -0600

    Update paths / dates

--- src/scripts/preprocessing/preprocessing_constants.py
+++ src/scripts/preprocessing/preprocessing_constants.py
@@ -102,7 +102,7 @@ datasets = {
                                      'plantation_type', 'SDPTv2', '20230911')
     },
     'sdpt': {
-        's3_raw': pp.join('plantations', 'sdpt_v3','sdpt_v3_vector_tiles','tiles_10d'),
+        's3_raw': pp.join('plantations', 'sdpt_v3','oct2025_updates','sdpt_by_tiles'),
         's3_processed_base': pp.join(processed_dir, 'sdpt'),
         's3_processed_small': pp.join(processed_dir, 'sdpt', 'chunks', today_date),
         's3_processed': pp.join(processed_dir, 'sdpt', today_date),
