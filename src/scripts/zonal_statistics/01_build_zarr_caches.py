1264e1f815293c04145841ae6eb41c1db82b738a
erin-glen <erin.glen@wri.org>
2025-11-16 15:27:55 -0600

    Updated docs

--- src/scripts/zonal_statistics/01_build_zarr_caches.py
+++ src/scripts/zonal_statistics/01_build_zarr_caches.py
@@ -2,8 +2,8 @@
 """Build aligned Zarr caches for organic-soils zonal statistics (canonical grid = pixel_area).
 
 python -m src.scripts.zonal_statistics.01_build_zarr_caches \
-  --interval_end_years 2021 \
-  --cluster_name zonal_stats \
+  --interval_end_years 2024 \
+  --cluster_name build_zarrs \
   --run_date 20251116 \
   --model_version 0_9_0 \
   --run_name ogh_sensitivity_1km \
