96983d9f54abb9861f0cce68ef8a4cc5409fd3f7
erin-glen <erin.glen@wri.org>
2025-11-17 21:08:10 -0700

    Restored doc

--- src/scripts/postprocessing/visualization/create_global_raster.py
+++ src/scripts/postprocessing/visualization/create_global_raster.py
@@ -26,7 +26,9 @@ Assumptions (this version)
 Examples
 --------
 # Aggregate to 0.01° on a running Dask cluster
-cond
+python -m src.scripts.postprocessing.visualization.create_global_raster \
+  -cn create_maps --run_name ogh_sensitivity_500m \
+  --model_version 0_9_5 --date_tag 20251117 --target_deg 0.01 --native_deg 0.00025
 
 # Aggregate at 0.01° using a local Dask scheduler (smaller local batch by default)
 AGG_LOCAL_BATCH=8 \
